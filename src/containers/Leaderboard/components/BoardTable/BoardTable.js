d1219014a9cb2a9bd37a3837e225f36044a4e6ca
Abhishek Kokate <abhishekkokate8@gmail.com>
2022-08-25 00:19:47 +0530

    Leaderboard table worked & pagination added

--- src/containers/Leaderboard/components/BoardTable/BoardTable.js
+++ src/containers/Leaderboard/components/BoardTable/BoardTable.js
@@ -1,32 +1,125 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowLeft,
+  faCaretLeft,
+  faCaretRight,
+  faStepBackward,
+  faStepForward,
+} from "@fortawesome/free-solid-svg-icons";
 import BoardTableRow from "../BoardTableRow/BoardTableRow";
 import "./BoardTable.css";
+import { useState } from "react";
 
 function BoardTable({ players }) {
+  const [currPage, setCurrPage] = useState(1);
+  const maxPages = Math.ceil(players?.length / 10);
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
+
+  useEffect(() => {
+    setFilteredPlayers(
+      players?.filter((e, i) => {
+        return i >= (currPage - 1) * 10 && i < (currPage - 1) * 10 + 10;
+      })
+    );
+  }, [currPage, maxPages]);
+
   return (
-    <div className="board-table-container">
-      <div className="board-table-title">
-        <div className="board-table-rank-no">
-          <h2>Rank</h2>
+    <>
+      <div className="board-table-container">
+        <div className="board-table-title">
+          <div className="board-table-rank-no">
+            <h2>Rank</h2>
+          </div>
+          <div className="board-table-rank-icon">
+            <h2></h2>
+          </div>
+          <div className="board-table-player">
+            <h2>Player</h2>
+          </div>
+          <div className="board-table-rr">
+            <h2>RR</h2>
+          </div>
+          <div className="board-table-wins">
+            <h2>Wins</h2>
+          </div>
         </div>
-        <div className="board-table-rank-icon">
-          <h2>Tier</h2>
-        </div>
-        <div className="board-table-player">
-          <h2>Player</h2>
-        </div>
-        <div className="board-table-rr">
-          <h2>RR</h2>
-        </div>
-        <div className="board-table-wins">
-          <h2>Wins</h2>
+        {filteredPlayers.map((e, i) => {
+          if (e?.leaderboardRank <= 3) return;
+          return <BoardTableRow player={e} key={i} />;
+        })}
+
+        <div className="board-table-navigation-container">
+          <a
+            href="javascript:void(0)"
+            className="navigator navigate-first"
+            onClick={() => {
+              setCurrPage((prev) => {
+                setCurrPage(1);
+              });
+            }}
+          >
+            <FontAwesomeIcon icon={faStepBackward} />
+          </a>
+          <a
+            href="javascript:void(0)"
+            className="navigator navigate-prev"
+            onClick={() => {
+              setCurrPage((prev) => {
+                if (prev <= 1) prev = 2;
+                console.log(prev);
+                setCurrPage(prev - 1);
+              });
+            }}
+          >
+            <FontAwesomeIcon icon={faCaretLeft} />
+          </a>
+          <div className="navigator" style={{ fontSize: "1rem" }}>
+            <input
+              type="text"
+              min={1}
+              max={maxPages}
+              name="pageno"
+              id="pageno"
+              value={currPage}
+              onChange={(e) => {
+                let val = e.target.value;
+                if (val > maxPages) {
+                  val = maxPages;
+                } else if (val < 1 || typeof val !== "Number") {
+                  val = 1;
+                }
+                setCurrPage(val);
+              }}
+            />
+            &nbsp;/&nbsp;{maxPages}
+          </div>
+          <a
+            href="javascript:void(0)"
+            className="navigator navigate-next"
+            onClick={() => {
+              setCurrPage((prev) => {
+                if (prev >= maxPages) prev = maxPages - 1;
+                setCurrPage(prev + 1);
+              });
+            }}
+          >
+            <FontAwesomeIcon icon={faCaretRight} />
+          </a>
+          <a
+            href="javascript:void(0)"
+            className="navigator navigate-last"
+            onClick={() => {
+              setCurrPage((prev) => {
+                setCurrPage(maxPages);
+              });
+            }}
+          >
+            <FontAwesomeIcon icon={faStepForward} />
+          </a>
         </div>
       </div>
-      {players?.map((e, i) => {
-        if (i >= 10 || i < 3) return;
-        return <BoardTableRow player={e} />;
-      })}
-    </div>
+    </>
   );
 }
 
